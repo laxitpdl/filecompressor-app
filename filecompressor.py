bf638d28ca13510ad2f0b135022825f833c77839
laxitpdl <poudellakshit042061@gmail.com>
2025-07-04 01:32:51 -0500

    save changes

--- filecompressor.py
+++ filecompressor.py
@@ -1,17 +1,31 @@
 import PySimpleGUI as fsg
+from zipcreator import make_archive
+
 
 label1 = fsg.Text("Select files you want to compress: ")
 input1 = fsg.Input()
-button1 = fsg.FileBrowse("Choose")
+button1 = fsg.FileBrowse("Choose", key = "files")
 
 label2 = fsg.Text("Select folder where you want to save: ")
 input2 = fsg.Input()
-button2 = fsg.FolderBrowse("Choose")
+button2 = fsg.FolderBrowse("Choose", key = "folder")
 
 button3 = fsg.Button("Compress")
+
+output_text = fsg.Text(key = "output", text_color="black")
+
 window = fsg.Window("File Compressor", [[label1, input1, button1],
                                         [label2, input2, button2],
-                                        [button3]])
+                                        [button3, output_text]])
 
-window.read()   
+ 
+while True:
+    event, values = window.read()
+    print (event, values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    make_archive (filepaths, folder)
+    window["output"].update(value="compression complete!")
+
+
 window.close()
